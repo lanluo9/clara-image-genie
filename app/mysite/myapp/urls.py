63e0f38641fc5e49157962b7aaf5933b836ba142
Chimdindu Chukwuka <cchimdindu@gmail.com>
2023-02-04 21:53:45 +0200

    update

--- app/mysite/myapp/urls.py
+++ app/mysite/myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.upload_folder,name='upload'),
     path('search/', views.search, name='search'),
-    path('upload/', views.upload_folder, name='upload'),
-    path('display/', views.display_images, name='display'),
+    #path('upload/', views.upload_folder, name='upload'),
+    #path('display/', views.display_images, name='display'),
 ]
