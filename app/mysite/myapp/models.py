3adb0bd290a473574a134e2a10f5db40556a99d2
sais14 <sais14@uw.edu>
2023-02-04 15:22:50 -0800

    updates

--- app/mysite/myapp/models.py
+++ app/mysite/myapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Image(models.Model):
     file_name = models.CharField(max_length=100)
     file_path = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.file_name
--- app/mysite/myapp/urls.py
+++ app/mysite/myapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('',views.upload_images,name='upload'),
     path('search/', views.search, name='search'),
-    #path('upload/', views.upload_folder, name='upload'),
-    #path('display/', views.display_images, name='display'),
+    path('display/', views.display_images, name='display'),
 ]
