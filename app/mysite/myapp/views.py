f2445bdc9c0f7013bf5096f91381900043bd104d
Chimdindu Chukwuka <cchimdindu@gmail.com>
2023-02-05 01:11:29 +0200

    update

--- app/mysite/myapp/views.py
+++ app/mysite/myapp/views.py
@@ -8,8 +8,6 @@ from django.shortcuts import render, redirect
 from .forms import ImageUploadForm
 
 
-
-#def search(request):
 def search(request):
     form = SearchForm(request.GET)
     if form.is_valid():
@@ -21,68 +19,29 @@ def search(request):
         form = SearchForm()
     return render(request, 'search.html', {'form': form})
 
-    # query = None
-    # images = []
-    # if 'query' in request.GET:
-    #     form = SearchForm(request.GET)
-    #     if form.is_valid():
-    #         query = form.cleaned_data['query']
-    #         images = Image.objects.filter(title__icontains=query)
-    # else:
-    #     form = SearchForm()
-    # return render(request, 'search.html', {'form': form, 'query': query, 'images': images})
-
-def home(request):
-    return render(request, 'home.html')
-
-# def upload_folder(request):
-#     if request.method == 'POST':
-#         files = request.FILES.getlist('folder')
-#         fs = FileSystemStorage()
-#         for file in files:
-#             filename = fs.save(file.name, file)
-#         return render(request, 'upload.html', {'uploaded_files': files})
-#     return render(request, 'upload.html')
-
 
 
 def display_images(request):
-    image_list = []
-    for filename in os.listdir(os.path.join(settings.MEDIA_ROOT, 'images')):
-        if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
-            image_list.append(filename)
-    return render(request, 'search.html', {'image_list': image_list})
-
-
-
-# def upload_images(request):
-#     if request.method == 'POST':
-#         form = ImageUploadForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('images')
-#     else:
-#         form = ImageUploadForm()
-#     return render(request, 'upload.html', {'form': form})
-
-
-# def upload_images(request):
-#     folder_path = '/path/to/folder'
-#     for filename in os.listdir(folder_path):
-#         file_path = os.path.join(folder_path, filename)
-#         file = Image(file_path=file_path)
-#         file.save()
-#     return render(request, 'upload.html', {'message': 'Folder uploaded successfully'})
+    #returns all images change to display select array of images
+    images = Image.objects.all()
+    return render(request, 'display.html', {'images': images})
 
 def upload_images(request):
     if request.method == 'POST':
-        uploaded_folder = request.FILES['folder']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_folder.name, uploaded_folder)
-        folder_path = fs.path(name)
-        for filename in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, filename)
-            file = Image(file_path=file_path)
-            file.save()
-        return render(request, 'upload.html', {'message': 'Folder uploaded successfully'})
-    return render(request, 'upload.html')
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_folder = request.FILES['folder']
+            fs = FileSystemStorage()
+            name = fs.save(uploaded_folder.name, uploaded_folder)
+            folder_path = fs.path(name)
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
+                file = Image(file_path=file_path, title=filename)
+                file.save()
+            return redirect('upload_images')
+    else:
+        form = ImageUploadForm()
+    return render(request, 'upload.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
